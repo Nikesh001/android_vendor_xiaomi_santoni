20da462005fafca1e52782ff60c97f2118c0be3b
Nikesh001 <nikkataria74@gmail.com>
2017-12-29 21:18:05 +0530

    santoni: remove telephony-ext

--- santoni/Android.mk
+++ santoni/Android.mk
@@ -309,14 +309,4 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := telephony-ext
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/framework/telephony-ext.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
 endif
